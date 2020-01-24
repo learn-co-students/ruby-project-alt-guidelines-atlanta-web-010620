39d23540090a167af1a5ce11e16188d62b84fceb
github username <github email address>
2020-01-24 11:49:12 -0500

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,12 +1,8 @@
 require_relative '../config/environment'
 
 system "clear"
-pastel = Pastel.new
 cli = CommandLineInterface.new
-old_logger = ActiveRecord::Base.logger
-ActiveRecord::Base.logger = nil
+
 
 cli.greet
 cli.log_in
-
-ActiveRecord::Base.logger = old_logger
